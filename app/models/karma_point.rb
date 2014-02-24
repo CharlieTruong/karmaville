728ab589bcaa2f1b5078483426a2155b79f565a3
Apprentice <apprentice@dbc-chicago-004.local>
2014-02-24 14:20:15 -0600

    added incrementing and decrementing the total karma points as the individual karma records are created

--- app/models/karma_point.rb
+++ app/models/karma_point.rb
@@ -5,4 +5,18 @@ class KarmaPoint < ActiveRecord::Base
   validates :user, :presence => true
   validates :value, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
   validates :label, :presence => true
+  after_create :increment_total_karma
+  after_destroy :decrement_total_karma
+
+  def increment_total_karma
+    total_karma_pt = TotalKarmaPt.find_by_user_id(user_id)
+    total_karma_pt.total_karma += value
+    total_karma_pt.save
+  end
+
+  def decrement_total_karma
+    total_karma_pt = TotalKarmaPt.find_by_user_id(user_id)
+    total_karma_pt.total_karma -= value
+    total_karma_pt.save
+  end
 end
