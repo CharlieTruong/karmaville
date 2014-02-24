583578f039b56d1c11d56ddd0b81284aa4d9e474
Apprentice <apprentice@dbc-chicago-004.local>
2014-02-24 14:30:40 -0600

    added pagination capability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.by_karma.limit(50)
+    if params[:page]
+      offset = (params[:page].to_i - 1) * 1000
+      @users = User.by_karma.limit(1000).offset(offset)
+    else
+      @users = User.by_karma.limit(1000)
+    end
   end
 end
