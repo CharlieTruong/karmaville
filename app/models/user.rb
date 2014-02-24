a26cc6dbd672c9975e49eb585ae529e221084aad
Apprentice <apprentice@dbc-chicago-004.local>
2014-02-24 11:23:57 -0600

    changed method to obtain total karma by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,18 @@ class User < ActiveRecord::Base
             :presence => true,
             :format => {:with => /^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/i},
             :uniqueness => {:case_sensitive => false}
-
+#selects top 50 aggregate karma_point objects
+#iterate over these
+#update a user's total karma with the total karma that got calculated here
   def self.by_karma
-    joins(:karma_points).group('users.id').order('SUM(karma_points.value) DESC')
+    # joins(:karma_points).group('users.id').order('SUM(karma_points.value) DESC')
+    karma_objs = KarmaPoint.select("user_id,sum(value) as total_karma").group(:user_id).order("SUM(value) DESC").limit(50)
+    users = karma_objs.map{|karma_obj| karma_obj.user}
   end
 
-  def total_karma
-    self.karma_points.sum(:value)
-  end
+  # def total_karma
+  #   self.karma_points.sum(:value)
+  # end
 
   def full_name
     "#{first_name} #{last_name}"
